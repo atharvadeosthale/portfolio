dc0291ef8fd10ea554b18850bc36b04abda28e19
Atharva Deosthale <atharva.deosthale17@gmail.com>
2024-01-16 19:58:39 +0530

    feat: start working on hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,18 @@ export default function Home() {
   return (
     <main className="max-w-6xl mx-auto">
       <Navbar />
+
+      <div className="flex justify-between items-center">
+        <div className="flex flex-col gap-5">
+          <p className="text-2xl font-mono">Hello, I'm</p>
+          <h1 className="font-mono text-4xl font-bold">Atharva Deosthale</h1>
+          <p className="font-mono text-xl">
+            I code and write content about it for a living.
+          </p>
+        </div>
+
+        <img src="/pfp.jpeg" className="rounded-lg w-52 h-52 object-cover" />
+      </div>
     </main>
   );
 }

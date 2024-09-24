075438fdef0406c6ff1d28259dfbf0fd48c4e150
Atharva Deosthale <atharva.deosthale17@gmail.com>
2024-09-24 20:30:54 +0530

    added redirects for yt, x, and blogs

--- next.config.js
+++ next.config.js
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/yt",
+        destination: "https://youtube.com/AtharvaDeosthale",
+        permanent: true,
+      },
+      {
+        source: "/x",
+        destination: "https://x.com/athudeosthale",
+        permanent: true,
+      },
+      {
+        source: "/blog",
+        destination: "https://blog.atharva.codes",
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

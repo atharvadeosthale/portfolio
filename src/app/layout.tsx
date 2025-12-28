620231b990d7c96d35482606b276383d56276a1d
Atharva Deosthale <atharva.deosthale17@gmail.com>
2025-12-28 18:17:28 +0530

    default theme should be system

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,7 +46,7 @@ export default function RootLayout({
       >
         <ThemeProvider
           attribute="class"
-          defaultTheme="dark"
+          defaultTheme="system"
           enableSystem
           disableTransitionOnChange
         >
